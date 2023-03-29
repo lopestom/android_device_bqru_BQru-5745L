a7cc1ac7a5e22473df6b475428214493331d8bba
lopestorm <guillopes2002@gmail.com>
2023-03-29 14:56:59 -0300

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 The TWRP Open Source Project
-#
-
-add_lunch_combo omni_BQru_5745L-userdebug
-#add_lunch_combo omni_BQru_5745L-eng
